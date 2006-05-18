d7e96178188b5adf0e06754150f71d40fe248b6d
nobody <nobody@6a21264b-131f-0410-8055-9322fb875141>
2006-05-18 17:35:11 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_1_prebranch'.

--- mentok/build3/autoconf/defines-autoconf.mk
+++
@@ -1,9 +0,0 @@
-#
-# include platform specific autoconf macros
-#
--include $(BS_ROOT)/autoconf/defines-autoconf-$(BS_PLATFORM_ARCH_FALLBACK_4).mk
--include $(BS_ROOT)/autoconf/defines-autoconf-$(BS_PLATFORM_ARCH_FALLBACK_3).mk
--include $(BS_ROOT)/autoconf/defines-autoconf-$(BS_PLATFORM_ARCH_FALLBACK_2).mk
--include $(BS_ROOT)/autoconf/defines-autoconf-$(BS_PLATFORM_ARCH_FALLBACK_1).mk
--include $(BS_ROOT)/autoconf/defines-autoconf-$(BS_PLATFORM_ARCH_FULL).mk
-
--- mentok/build3/codegen/defines-codegen.mk
+++
@@ -1,7 +0,0 @@
--include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_4).mk
--include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_3).mk
--include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_2).mk
--include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_1).mk
--include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FULL).mk
-
-
