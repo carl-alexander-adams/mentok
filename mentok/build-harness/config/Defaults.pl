01b798642e76bbea195cfc4b1daca6a380e69042
hallm4 <hallm4@6a21264b-131f-0410-8055-9322fb875141>
2006-05-23 21:41:25 +0000

    Reworked conf files to be a bit cleaner - much easier to identify where vars are coming from.

--- mentok/build-harness/config/Defaults.pl
+++ mentok/build-harness/config/Defaults.pl
@@ -1,67 +1,58 @@
 
 ###########################################################
 ###
-### Defaults to be sourced in by buildall and runbuild
+### Environment
 ###
 ###########################################################
 
-$ENV{'PATH'} = "/bin:/usr/bin:/usr/local/bin:/usr/ucb:/usr/ccs/bin:/sbin:.:$ENV{'PATH'}";
-
-# New build3.5 feature - we want verbose for tracking down errors.
+$ENV{'PATH'}       = "/bin:/usr/bin:/usr/local/bin:/usr/ucb:/usr/ccs/bin:/sbin:.";
 $ENV{'BS_VERBOSE'} = 2;
-
 umask(022);
 
 ###########################################################
-### Needed for Sun compilers and Purify
+###
+### General info we find and cache for later use. 
+###
 ###########################################################
 
-$ENV{'LM_LICENSE_FILE'} = '1726@<somehostname>';
+($sysname, $nodename, $release, $version, $machine) = POSIX::uname();
 
-###########################################################
-### From buildall
-###########################################################
+$defaults{'uname'} = "$sysname $release $machine";
 
-$uname    = 'uname -smr';
+$arch     = $sysname; $arch =~ s/ /\-/g;
+$defaults{'arch'}  = $arch;
 
-$ping      = get_ping(); 
-$cp        = get_cp();
-$ssh       = get_ssh(); 
+$host     = hostname(); $host =~ m#^([A-Za-z0-9_-]*)#; $host = $1;
+$defaults{'host'} = $host;
 
-$hostname = 'hostname';
-$mkdir    = '/bin/mkdir -p -m 0755';
-$gmake    = '/usr/local/bin/gmake';
-$sudo     = '/usr/local/bin/sudo';
-$p4       = '/usr/local/bin/p4';
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time());
+$defaults{'year'}  = $year + 1900;
+$defaults{'month'} = ($mon < 10 ? $mon = "0" . $mon : $mon);
+$defaults{'month'}++;
+$defaults{'day'}   = ($mday < 10 ? $mday = "0" . $mday : $mday); 
 
-###########################################################
-### Vars used by the Web CGI scripts
-### These may be over-written by product config files
-###########################################################
-#$cgi_bcmd = "/home/builds/scripts/buildall.pl";
+$defaults{'transport'} = get_ssh(); 
+$defaults{'transport_port'} = 22;
 
-###########################################################
-### derived stuff
-###########################################################
-
-chomp($arch = `$uname`); $arch =~ s/ /\-/g;
-chomp($host = `$hostname | cut -d. -f1`);
-$numcpu     = get_proc_info ($arch);
+$defaults{'make'}      = '/usr/local/bin/gmake';
 
 ###########################################################
 ### From $project config
 ###########################################################
-$toolsdir     = '/home/builds/scripts';
-$buildtool    = "$toolsdir/runbuild.pl";
-$buildreport  = "$toolsdir/buildreport.pl";
-$platinfo     = "$toolsdir/platforminfo.pl -v";
-$platlinks    = "$toolsdir/makeplatformlinks.pl";
-$wbtb         = "$toolsdir/wbtb.pl";
-$alarmwait    = 60*180; # 180 mins
-$cmdline      = "$buildtool";
-$dateVer      = getDateVersion();
 
-$PRUNE_AFTER = 5;
+$defaults{'alarmwait'}    = 60 * 180; # 180 mins
+$defaults{'toolsdir'}     = '/home/builds/scripts';
+$defaults{'buildtool'}    = "$defaults{'toolsdir'}/runbuild.pl";
+$defaults{'buildreport'}  = "$defaults{'toolsdir'}/buildreport.pl";
+$defaults{'wbtb'}         = "$defaults{'toolsdir'}/wbtb.pl";
+$defaults{'cmdline'}      =  $defaults{'buildtool'};
+$defaults{'prune_after'}  = 5;
+
+$defaults{'mailserver'}   = "mail";
+$defaults{'sender'}       = "build-user";
+### We append $projectname/summary.html to baseurl later.
+$defaults{'baseurl'}      = "http://devserver/builds";
+
 
 ###########################################################
 ### This will be the default client build
@@ -72,87 +63,12 @@ $PRUNE_AFTER = 5;
 ### [dir] [sudo] [command] [args] [targets]
 ###########################################################
 
-@client_build_sequence = (
-        [ "", "", "$gmake", "", "" ],
-        [ "", "", "$gmake", "", "package" ],
-);
-
-
-# re - release engineering
-$re = '<a href="mailto:email@somewhere">Set your Email Address for RE</a>';
+### define this in product config 
+@client_build_sequence = ( ); 
 
 ###########################################################
 ### FUNCTIONS
 ###########################################################
-
-sub get_proc_info {
-   
-   my $arch   = shift;
-   my $numcpu = 1;
-
-   if ($arch =~ /SunOS/) {
-      chomp($numcpu = `uname -X | grep NumCPU | awk -F" " '{print \$3}'`);
-   } elsif ($arch =~ /Linux/) {
-      chomp($numcpu = `cat /proc/cpuinfo | grep processor | wc -l`);
-   }
-
-   return $numcpu;
-
-}
-
-###########################################################
-
-sub getDateVersion {
-
-   my $date = `date +%Y-%m-%d`;
-   chomp ($date);
-   $date = $date || "UNKNOWN";
-   return $date;
-
-}
-
-sub get_ping {
-
-   my $arch  = '';
-   my $pargs = '';
-   my $ping  = '';
-
-   chomp($arch = `$uname`); $arch =~ s/ /\-/g;
-
-   if ($arch =~ /Linux/) {
-      $pargs = " -c 1 ";
-   }
-
-   my @locations = qw(/usr/sbin/ping /bin/ping /sbin/ping);
-
-   foreach (@locations) {
-      if ( -x $_ )  {
-         $ping = $_;
-         last;
-      }
-   }
-
-   return ($ping . $pargs);
-
-}
-
-
-sub get_cp {
-
-   my $cp = '';
-
-   my @locations = qw(/usr/bin/cp /bin/cp);
-
-   foreach (@locations) {
-      if ( -x $_ )  {
-         $cp  = $_;
-         last;
-      }
-   }
-                                                                                      
-   return ($cp);
-
-}
 
 sub get_ssh {
 
