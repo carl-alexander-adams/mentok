207873e84e901db467d3491b1458f90e5f2256b0
carlalex <carlalex@6a21264b-131f-0410-8055-9322fb875141>
2007-01-19 22:07:48 +0000

    More window porting work cleanup of fallback platforms to better work with the improved runtime version code in plat.pl

--- trunk/mentok/build3-sample-component-tree/sample-srcdir/sample-nativecode/print-runtime-version.h
+++
@@ -1,6 +0,0 @@
-#ifndef _PRINT_RUNTIME_VERSION_H_
-#define _PRINT_RUNTIME_VERSION_H_
-
-void print_runtime_version(void);
-
-#endif /* _PRINT_RUNTIME_VERSION_H_ */
--- trunk/mentok/build3/codegen/defines-codegen.mk
+++ trunk/mentok/build3/codegen/defines-codegen.mk
@@ -1,3 +1,11 @@
+-include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_12).mk
+-include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_11).mk
+-include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_10).mk
+-include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_9).mk
+-include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_8).mk
+-include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_7).mk
+-include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_6).mk
+-include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_5).mk
 -include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_4).mk
 -include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_3).mk
 -include $(BS_ROOT)/codegen/defines-codegen-$(BS_PLATFORM_ARCH_FALLBACK_2).mk
